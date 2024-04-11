1eed05292a93713f751894a2ce32f7eaea744b0a
Juan Carlos Avila <juanvs23@gmail.com>
2024-04-11 13:02:33 -0400

    initital

---
+++ coltman-seguro.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CRM SEGUIMIENTO DE POLIZA DE SEGURO
+ *
+ * @package           ColtmanSeguro
+ * @author            Juan Carlos Avila Pérez
+ * @copyright         2024 Juan Carlos Avila Pérez
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       CRM Seguimiento de Póliza de Seguro
+ * Plugin URI:        https://github.com/juanvs23/coltman-crm-seguro
+ * Description:       Customer Relationship Management desarrollado para la gestión de pólizas de seguro.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Juan Carlos Avila Pérez
+ * Author URI:        https://github.com/juanvs23
+ * Text Domain:       coltman_seguro
+ * License:           GPL v2 or later
+ */
